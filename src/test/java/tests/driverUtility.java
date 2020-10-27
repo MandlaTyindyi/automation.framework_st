d119bbd42efa64c0c722e775a07fc3dadce7f9a1
Mkhuseli MPU <Mkhuseli MPU@10.0.0.49>
2020-10-27 11:29:23 +0200

    initial commit

---
+++ src/test/java/tests/driverUtility.java
@@ -0,0 +1,154 @@
+package tests;
+
+import org.openqa.selenium.By;
+import java.util.Random;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class driverUtility {
+	Random rand;
+	 /**
+     * Functions to be used in the test 
+     */
+    public boolean waitForElement(String xPath, WebDriver driver)
+    {
+        boolean elementFound;
+        System.out.println("INFO: Waiting for element: "+xPath);
+        try
+        {
+            elementFound = false;
+            while(elementFound != true)
+            {
+                WebDriverWait wait = new WebDriverWait(driver, 1);
+                while(wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xPath)))!=null)
+                {
+                    elementFound = true;
+                    break;
+                }
+                
+                return elementFound;
+            }
+        } catch (Exception e)
+        {
+            elementFound = false;
+            System.out.println("ERROR: There was an error while waiting for element: "+xPath);
+        }
+        
+        return elementFound;
+    }
+    
+    
+    public boolean scrollToElement(String elementXpath, WebDriver driver)
+    {
+        try
+        {
+            WebElement element = driver.findElement(By.xpath(elementXpath));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            System.err.println("Error scrolling to element - " + elementXpath + " - " + e.getStackTrace());
+            return false;
+        }
+
+    }
+    
+    public boolean clickElementbyXpath(String elementXpath, WebDriver driver)
+    {
+        try
+        {
+            System.out.println("INFO: Clicking element by Xpath : " + elementXpath);
+            waitForElement(elementXpath,driver);
+            WebDriverWait wait = new WebDriverWait(driver, 5);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath(elementXpath)));
+            WebElement elementToClick = driver.findElement(By.xpath(elementXpath));
+            elementToClick.click();
+            return true;
+        }
+        catch (Exception e)
+        {
+            System.err.println("[Error] Failed to click on element by Xpath - " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public boolean enterTextByXpath(String elementXpath, WebDriver driver, String textToEnter)
+    {
+        try
+        {
+            waitForElement(elementXpath,driver);
+            WebElement elementToTypeIn = driver.findElement(By.xpath(elementXpath));
+            elementToTypeIn.sendKeys(Keys.chord(Keys.CONTROL, "a"), textToEnter);
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            System.err.println("Error entering text - " + elementXpath + " - " + e.getMessage());
+            return false;
+        }
+    }
+    
+    public boolean selectRadioButtonUsingElementXpath(String radioButtonXpath, WebDriver driver)
+    {
+        try
+        {
+            WebElement radioButton = driver.findElement(By.xpath(radioButtonXpath));
+            radioButton.click();
+            
+        }
+        catch (Exception ex)
+        {
+            System.err.println("[ERROR] failed to find element: " + ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+    
+    public String switchToNewWindow(WebDriver driver)
+    {
+        // get the current window handle
+        String parentHandle = driver.getWindowHandle();
+        while (parentHandle.equalsIgnoreCase(driver.getWindowHandle()))
+        {
+            for (String winHandle : driver.getWindowHandles())
+            {
+
+                // switch focus of WebDriver to the next found window handle (that's your newly opened window)
+                driver.switchTo().window(winHandle);
+            }
+
+        }
+
+        return parentHandle;
+    }
+    
+    public void pause(){
+        try{
+            Thread.sleep(3000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String generatePhoneNumber() {
+    	Random rand = new Random();
+
+	    int num1, num2, num3, num11;
+
+	    num1 = rand.nextInt (3) + 6;
+	    num11= rand.nextInt(9);
+	    num2 = rand.nextInt (643) + 100;
+	    num3 = rand.nextInt (9000) + 1000;
+
+	   return 0+""+num1+num11+" "+num2+" "+num3;
+	   
+    }
+
+}
---
+++ src/test/java/tests/testingInChrome.java
@@ -0,0 +1,189 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import tests.driverUtility;
+
+
+public class testingInChrome {
+	WebDriver driver;
+	driverUtility driverUtil;
+	
+    /**
+    * The page objects 
+    */
+    public static String  careersLinkXpath()
+    {
+        return "//li[@id='menu-item-1373']//a[text()='CAREERS']";
+    }
+    public static String saLinkXpath()
+    {
+        return "//a[text()='South Africa']";
+    }
+    
+    public static String  firstJobLinkXpath()
+    {
+        return "//a[text()='Interns - BSC Computer Science, National Diploma: IT Development Graduates']";
+    }
+    public static String  applyOnlineLinkXpath()
+    {
+        return "//a[contains(text(), 'Apply Online')]";
+    }
+    public static String  nameXpath()
+    {
+        return "//input[@id='applicant_name']";
+    }
+    
+    
+    
+    public static String  emailXpath()
+    {
+        return "//input[@id='email']";
+    }
+    public static String  phoneXpath()
+    {
+        return "//input[@id='phone']";
+    }
+    public static String  textAreaXpath()
+    {
+        return "//textarea[@id='message']";
+    }
+    public static String  textValidationXpath()
+    {
+        return "//li[text()='You need to upload at least one file.']";
+    }
+    public static String  sendApplicationButtonXpath()
+    {
+        return "//input[@value='Send Application']";
+    }
+  
+    @BeforeTest
+	public void setup(){
+    	 String exePath = "C:\\Users\\Mkhuseli MPU\\Desktop\\mandla\\s.tyindyi_frmwrk\\dataDrivenAutomationFramework\\src\\test\\resources\\drivers\\chromedriver.exe";
+	     System.setProperty("webdriver.chrome.driver", exePath);
+	     String URL = "https://www.ilabquality.com/";
+	     driver = new ChromeDriver();
+	     driver.get(URL);
+	     driver.manage().window().maximize();
+	     driverUtil = new driverUtility();
+	       
+	}
+    
+    @Test
+	public void chromeTest(){
+    	
+    	System.out.println("Test started...");
+    	if(!(driverUtil.waitForElement(careersLinkXpath(),driver))){
+            System.out.println("Failed to wait for the Careers link");
+        }
+        
+        if(!(driverUtil.clickElementbyXpath(careersLinkXpath(),driver))){
+            System.out.println("Failed to click for the Careers link");
+        }
+        
+        if(!(driverUtil.scrollToElement(saLinkXpath(),driver))){
+        	if(!(driverUtil.scrollToElement(saLinkXpath(),driver))){
+        		if(!(driverUtil.scrollToElement(saLinkXpath(),driver))){
+                    System.out.println("Failed to scroll to SA link");
+                }
+            }
+        }
+        driverUtil.pause();
+        
+        if(!(driverUtil.clickElementbyXpath(saLinkXpath(),driver))){
+            System.out.println("Failed to click on SA link");
+        }
+        
+        if(!(driverUtil.scrollToElement(firstJobLinkXpath(),driver))){
+        	if(!(driverUtil.scrollToElement(firstJobLinkXpath(),driver))){
+        		if(!(driverUtil.scrollToElement(firstJobLinkXpath(),driver))){
+                    System.out.println("Failed to scroll to first job link");
+                }
+            }
+        }
+        driverUtil.pause();
+        
+        if(!(driverUtil.clickElementbyXpath(firstJobLinkXpath(),driver))){
+            System.out.println("Failed to click on the first link");
+        }
+        
+        if(!(driverUtil.scrollToElement(applyOnlineLinkXpath(),driver))){
+        	if(!(driverUtil.scrollToElement(applyOnlineLinkXpath(),driver))){
+        		if(!(driverUtil.scrollToElement(applyOnlineLinkXpath(),driver))){
+                    System.out.println("Failed to scroll to apply online button");
+                }
+            }
+        }
+        driverUtil.pause();
+        
+        if(!(driverUtil.clickElementbyXpath(applyOnlineLinkXpath(),driver))){
+            System.out.println("Failed to click on the apply online button");
+        }
+        
+        if(!(driverUtil.scrollToElement(nameXpath(),driver))){
+        	if(!(driverUtil.scrollToElement(nameXpath(),driver))){
+        		if(!(driverUtil.scrollToElement(nameXpath(),driver))){
+                    System.out.println("Failed to scroll to the name field");
+                }
+            }
+        }
+        driverUtil.pause();
+        if(!(driverUtil.enterTextByXpath(nameXpath(), driver, "InputFirstName"))){
+    		if(!(driverUtil.enterTextByXpath(emailXpath(), driver, "InputFirstName"))){
+                System.out.println("Failed to enter name");
+            }
+        }
+        
+        if(!(driverUtil.enterTextByXpath(emailXpath(), driver, "automationAssessment@iLABQuality.com"))){
+    		if(!(driverUtil.enterTextByXpath(emailXpath(), driver, "automationAssessment@iLABQuality.com"))){
+                System.out.println("Failed to enter email address");
+            }
+        }
+        
+        if(!(driverUtil.enterTextByXpath(phoneXpath(), driver, driverUtil.generatePhoneNumber()))){
+    		if(!(driverUtil.enterTextByXpath(phoneXpath(), driver, driverUtil.generatePhoneNumber()))){
+                System.out.println("Failed to enter phone number");
+            }
+        }
+        
+        if(!(driverUtil.enterTextByXpath(textAreaXpath(), driver, "TextArea"))){
+    		if(!(driverUtil.enterTextByXpath(textAreaXpath(), driver, "TextArea"))){
+                System.out.println("Failed to text into the text area");
+            }
+        }
+        
+        if(!(driverUtil.clickElementbyXpath(sendApplicationButtonXpath(),driver))){
+            System.out.println("Failed to click on send application button");
+        }
+        driverUtil.pause();
+        if(!(driverUtil.scrollToElement(textValidationXpath(),driver))){
+        	if(!(driverUtil.scrollToElement(textValidationXpath(),driver))){
+        		if(!(driverUtil.scrollToElement(textValidationXpath(),driver))){
+                    System.out.println("Failed to scroll to the validation text");
+                }
+            }
+        }
+        if(!(driverUtil.waitForElement(textValidationXpath(), driver))){
+            System.out.println("Failed to validate text");
+        }
+        WebElement elementToValidate = driver.findElement(By.xpath(textValidationXpath()));
+        String expectedMessage = "You need to upload at least one file.";
+        String actualMessage = elementToValidate.getText();
+        Assert.assertEquals(actualMessage, expectedMessage);
+    	driverUtil.pause();
+    	System.out.println("Test completed...");	
+	}
+    
+    @AfterTest
+	public void teardown(){
+		driver.close();
+		driver.quit();
+	}
+
+}
